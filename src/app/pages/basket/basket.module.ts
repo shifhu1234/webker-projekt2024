611f2831010bbc169eb7c521cf436373708b07a0
Nagy Gábor <h267930@stud.u-szeged.hu>
2024-05-02 18:11:11 +0200

    Kosar hozaadasa

---
+++ src/app/pages/basket/basket.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BasketRoutingModule } from './basket-routing.module';
+
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    CommonModule,
+    BasketRoutingModule
+  ]
+})
+export class BasketModule { }
