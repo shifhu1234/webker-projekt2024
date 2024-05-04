ebf9cf3b90da727569edda9e2ed78e29c2026cb3
Nagy Gábor <h267930@stud.u-szeged.hu>
2024-05-04 11:13:35 +0200

    changes

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -8,96 +8,80 @@ import {MatButtonModule} from "@angular/material/button";
 import {Observable} from "rxjs";
 
 @Component({
-  selector: 'app-products',
-  standalone: true,
-  imports: [
-    MatCardModule,
-    NgForOf,
-    FlexModule,
-    MatButtonModule,
-    NgIf
-  ],
-  templateUrl: './products.component.html',
-  styleUrl: './products.component.scss'
+    selector: 'app-products',
+    standalone: true,
+    imports: [
+        MatCardModule,
+        NgForOf,
+        FlexModule,
+        MatButtonModule,
+        NgIf
+    ],
+    templateUrl: './products.component.html',
+    styleUrl: './products.component.scss'
 })
 
 export class ProductsComponent implements OnInit, OnChanges {
-  categories = ['Gyümölcs', 'Zöldség', 'Méz', 'Szárazáru'];
-  categoriesName = ['fruit', 'vegetable', 'honey', 'pasta'];
-  products?: Products[];
-  categoryImages: string[] = [];
-  categoryImagesFiltered: string[] = [];
-  isFilterActive: boolean = false;
+    categories = ['Gyümölcs', 'Zöldség', 'Méz', 'Szárazáru'];
+    categoriesName = ['fruit', 'vegetable', 'honey', 'pasta'];
+    products?: Products[];
+    categoryImages: string[] = [];
+    categoryImagesFiltered: string[] = [];
+    isFilterActive: boolean = false;
 
-  @Output() selectedCategory: EventEmitter<string> = new EventEmitter<string>();
-  @Input() filteredProducts: Products[] = [];
+    @Output() selectedCategory: EventEmitter<string> = new EventEmitter<string>();
+    @Input() filteredProducts: Products[] = [];
 
-  constructor(private productsService: ProductsService) {
-  }
+    constructor(private productsService: ProductsService) {
+    }
 
-  ngOnChanges(): void {
+    ngOnChanges(): void {
 
-  }
+    }
 
-  teszt?: Products[];
+    ngOnInit(): void {
+        this.loadCategoryImages();
+        this.productsService.getProducts().subscribe((data: Products[]) => {
+            this.products = data;
 
-  ngOnInit(): void {
-    this.loadCategoryImages();
-    this.productsService.getProducts().subscribe((data: Products[]) => {
-      this.products = data;
+            console.log("data: " + data);
 
+        });
+    }
 
-      for (const a of data) {
-        console.log(a.name);
-      }
-    });
+    private loadCategoryImages(): void {
+        this.categoriesName.forEach(category => {
+            const imageUrl = `assets/${category}Kategoria.jpg`;
+            this.categoryImages.push(imageUrl);
+        });
+    }
 
+    private loadFilteredCategoryImages(categoryType: string): void {
+        this.categoryImagesFiltered = [];
+        this.filteredProducts.forEach(category => {
+            const imageUrl = `assets/img${categoryType.charAt(0).toUpperCase()}${categoryType.slice(1)}/${category.image_url}`;
+            //console.log(imageUrl);
+            this.categoryImagesFiltered.push(imageUrl);
+            //console.log("kateg " + category.image_url);
+        })
+    }
 
-    this.productsService.getFruitProducts().subscribe((data: Products[]) => {
-      this.teszt = data;
+    onSelectCategory(category: string): void {
+        this.filteredProducts = [];
+        for (const item of this.products as any)
+            if (category as string === item.type as string) {
+                this.filteredProducts.push(item); // Add matching products to the filtered list
+                console.log(item.name)
+            }
+        this.isFilterActive = true;
+        this.loadFilteredCategoryImages(category);
+        this.selectedCategory.emit(category);
+        // console.log(this.filteredProducts.length)
+        //   return this.filteredProducts.length;
+    }
 
-      for (const a of data) {
-        console.log("teszt: " + a.name);
-      }
-
-
-    });
-
-  }
-
-  private loadCategoryImages(): void {
-    this.categoriesName.forEach(category => {
-      const imageUrl = `assets/${category}Kategoria.jpg`;
-      this.categoryImages.push(imageUrl);
-    });
-  }
-
-  private loadFilteredCategoryImages(categoryType: string): void {
-    this.categoryImagesFiltered = [];
-    this.filteredProducts.forEach(category => {
-      const imageUrl = `assets/img${categoryType.charAt(0).toUpperCase()}${categoryType.slice(1)}/${category.image_url}`;
-      //console.log(imageUrl);
-      this.categoryImagesFiltered.push(imageUrl);
-      //console.log("kateg " + category.image_url);
-    })
-  }
-
-  onSelectCategory(category: string): void {
-    this.filteredProducts = [];
-    for (const item of this.products as any)
-      if (category as string === item.type as string) {
-        this.filteredProducts.push(item); // Add matching products to the filtered list
-        console.log(item.name)
-      }
-    this.isFilterActive = true;
-    this.loadFilteredCategoryImages(category);
-    this.selectedCategory.emit(category);
-    // console.log(this.filteredProducts.length)
-    //   return this.filteredProducts.length;
-  }
-
-  goBackToCategoires() {
-    this.filteredProducts = [];
-    this.isFilterActive = false;
-  }
+    goBackToCategoires() {
+        this.filteredProducts = [];
+        this.isFilterActive = false;
+    }
 }
--- src/app/shared/services/products.service.ts
+++ src/app/shared/services/products.service.ts
@@ -2,41 +2,25 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {map, Observable} from "rxjs";
 import {Products} from "../models/Products";
+import {AngularFirestore} from "@angular/fire/compat/firestore";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor(private http: HttpClient) {
-  }
+    constructor(private http: HttpClient, private afs: AngularFirestore) {
+    }
 
-  getProducts(): Observable<Products[]> {
-    return this.http.get<Products[]>('assets/products.json');
-  }
+    collectionName = 'images';
 
-  // Gyümölcs termékek lekérése
-  getFruitProducts(): Observable<Products[]> {
-    return this.getProducts().pipe(
-      map(products => products.filter(product => product.type === 'fruit'))
-    );
-  }
-  getVegetableProducts(): Observable<Products[]> {
-    return this.getProducts().pipe(
-      map(products => products.filter(product => product.type === 'vegetable'))
-    );
-  }
+    getProducts(): Observable<Products[]> {
+        return this.http.get<Products[]>('assets/products.json');
+    }
 
-  getHoneyProducts(): Observable<Products[]> {
-    return this.getProducts().pipe(
-      map(products => products.filter(product => product.type === 'honey'))
-    );
-  }
-  getPastaProducts(): Observable<Products[]> {
-    return this.getProducts().pipe(
-      map(products => products.filter(product => product.type === 'pasta'))
-    );
-  }
-
+    getAfsProducts(): Observable<Products[]>{
+        return this.afs.collection<Products>(this.collectionName).valueChanges();
+        //return this.afs.collection<Products>(`${this.collectionName}/imgFruit`).valueChanges();
+    }
 
 }
