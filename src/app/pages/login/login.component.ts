0789a4d4af8b6ba59ac300d720c922cc7ddb619a
Nagy Gábor <h267930@stud.u-szeged.hu>
2024-05-09 17:26:48 +0200

    error fix

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -27,45 +27,7 @@ export class LoginComponent implements OnInit, OnDestroy {
 
     async login() {
         this.loading = true;
-        // Promise
-        /* this.loadingService.loadingWithPromise(this.email.value, this.password.value).then((_: boolean) => {
-          console.log('This executed second.');
-          this.router.navigateByUrl('/main');
-        }).catch(error => {
-          console.error(error, 'Incorrect email or password!');
-        }).finally(() => {
-          console.log('this is executed finally.');
-        }); */
 
-        // async-await
-        /* try {
-          // then
-          const _ = await this.loadingService.loadingWithPromise(this.email.value, this.password.value)
-          this.router.navigateByUrl('/main');
-        } catch (error) {
-          // catch
-          console.error(error, 'Incorrect email or password!');
-        }
-        // finally
-        console.log('this is executed finally.'); */
-
-        // Observable
-        // memory leak
-        // this.loadingObservation = this.loadingService.loadingWithObservable(this.email.value as string, this.password.value as string)
-        // this.loadingSubscription = this.loadingObservation
-        //   .subscribe(
-        //     {
-        //       next: (data: boolean) => {
-        //         //console.log(data);
-        //         this.router.navigateByUrl('/main');
-        //         this.loading = false;
-        //       }, error: (error) => {
-        //         console.error(error);
-        //       }, complete: () => {
-        //         console.log('finally');
-        //       }
-        //     }
-        //   );
 
         this.authService.login(this.email.value as string, this.password.value as string).then(cred => {
             //console.log(cred);
@@ -84,3 +46,42 @@ export class LoginComponent implements OnInit, OnDestroy {
     }
 
 }
+// Promise
+/* this.loadingService.loadingWithPromise(this.email.value, this.password.value).then((_: boolean) => {
+  console.log('This executed second.');
+  this.router.navigateByUrl('/main');
+}).catch(error => {
+  console.error(error, 'Incorrect email or password!');
+}).finally(() => {
+  console.log('this is executed finally.');
+}); */
+
+// async-await
+/* try {
+  // then
+  const _ = await this.loadingService.loadingWithPromise(this.email.value, this.password.value)
+  this.router.navigateByUrl('/main');
+} catch (error) {
+  // catch
+  console.error(error, 'Incorrect email or password!');
+}
+// finally
+console.log('this is executed finally.'); */
+
+// Observable
+// memory leak
+// this.loadingObservation = this.loadingService.loadingWithObservable(this.email.value as string, this.password.value as string)
+// this.loadingSubscription = this.loadingObservation
+//   .subscribe(
+//     {
+//       next: (data: boolean) => {
+//         //console.log(data);
+//         this.router.navigateByUrl('/main');
+//         this.loading = false;
+//       }, error: (error) => {
+//         console.error(error);
+//       }, complete: () => {
+//         console.log('finally');
+//       }
+//     }
+//   );
