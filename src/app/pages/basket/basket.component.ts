77312b8422a26c566fab2349f350a2a4ded823ff
Nagy Gábor <h267930@stud.u-szeged.hu>
2024-05-11 15:53:57 +0200

    pop up fizetes

--- src/app/pages/basket/basket.component.ts
+++ src/app/pages/basket/basket.component.ts
@@ -8,66 +8,79 @@ import {CouponsFormatPipe} from "../../shared/pipes/coupons-format.pipe";
 import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
 import {MatFormField, MatInput, MatLabel} from "@angular/material/input";
 import {ReactiveFormsModule} from "@angular/forms";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, RouterLink} from "@angular/router";
 import {MatButton} from "@angular/material/button";
+import {MatDialog} from "@angular/material/dialog";
+import {PopUpTransactionComponent} from "../../shared/pop-ups/pop-up-transaction/pop-up-transaction.component";
 
 @Component({
-  selector: 'app-basket',
-  standalone: true,
-  imports: [
-    NgForOf,
-    CouponsFormatPipe,
-    MatCard,
-    MatCardContent,
-    MatCardHeader,
-    MatCardTitle,
-    MatFormField,
-    MatInput,
-    MatLabel,
-    ReactiveFormsModule,
-    MatButton,
-    NgIf
-  ],
-  templateUrl: './basket.component.html',
-  styleUrl: './basket.component.scss'
+    selector: 'app-basket',
+    standalone: true,
+    imports: [
+        NgForOf,
+        CouponsFormatPipe,
+        MatCard,
+        MatCardContent,
+        MatCardHeader,
+        MatCardTitle,
+        MatFormField,
+        MatInput,
+        MatLabel,
+        ReactiveFormsModule,
+        MatButton,
+        NgIf,
+        RouterLink
+    ],
+    templateUrl: './basket.component.html',
+    styleUrl: './basket.component.scss'
 })
 export class BasketComponent implements OnInit, OnChanges {
-  basketTransactions: Transaction[] = [];
-  totalAmount: number = 0;
-  loggedInUser?: firebase.default.User | null;
-  userId?: string;
-  discount: number = 0;
-  totalAmountNoCoupons: number = 0;
-  constructor(private basketService: BasketService, private userService: UserService, private appComponent: AppComponent, private route: ActivatedRoute) {
-  }
+    basketTransactions: Transaction[] = [];
+    totalAmount: number = 0;
+    loggedInUser?: firebase.default.User | null;
+    userId?: string;
+    discount: number = 0;
+    totalAmountNoCoupons: number = 0;
 
-  ngOnChanges(): void {
-    // this.discount = 0;
-  }
-
-  ngOnInit(): void {
-    this.loggedInUser = this.appComponent.getLoggedInUser();
-    if (this.loggedInUser) {
-      // console.log("uid " + this.loggedInUser.uid)
-      this.userId = this.loggedInUser.uid;
-      this.basketTransactions = this.basketService.getBasketTransactions(this.userId);
-      this.calculateTotalAmount();
+    constructor(private basketService: BasketService, private userService: UserService, private appComponent: AppComponent, private route: ActivatedRoute, private dialogRef: MatDialog) {
     }
-    this.route.queryParams.subscribe(params => {
-      this.discount = +params['discount'];
-      this.totalAmount = this.totalAmountNoCoupons * (1-this.discount)
-    });
-  }
 
-  calculateTotalAmount() {
-    let totalAmount = 0;
-    for (const transaction of this.basketTransactions) {
-      totalAmount += transaction.totalPrice;
+    ngOnChanges(): void {
+        // this.discount = 0;
     }
-    this.totalAmountNoCoupons = totalAmount;
-  }
 
-  removeCoupon() {
-    this.discount = 0;
-  }
+    ngOnInit(): void {
+        this.loggedInUser = this.appComponent.getLoggedInUser();
+        if (this.loggedInUser) {
+            // console.log("uid " + this.loggedInUser.uid)
+            this.userId = this.loggedInUser.uid;
+            this.basketTransactions = this.basketService.getBasketTransactions(this.userId);
+            this.calculateTotalAmount();
+        }
+        this.route.queryParams.subscribe(params => {
+            this.discount = +params['discount'];
+            this.totalAmount = this.totalAmountNoCoupons * (1 - this.discount)
+        });
+    }
+
+    openTransactionDialog(): void {
+        this.dialogRef.open(PopUpTransactionComponent, {
+            width: '20%',
+            data: {
+                totalAmount: this.totalAmountNoCoupons
+            }
+        });
+    }
+
+    calculateTotalAmount() {
+        let totalAmount = 0;
+        for (const transaction of this.basketTransactions) {
+            totalAmount += transaction.totalPrice;
+        }
+        this.totalAmountNoCoupons = totalAmount;
+    }
+
+    removeCoupon() {
+        this.discount = 0;
+    }
 }
